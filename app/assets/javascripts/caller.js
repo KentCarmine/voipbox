21dd5680b3bbbd4ca6c072c16b8f9853acf0cb56
dbc-apprentice <apprentice@devbootcamp.com>
2014-01-13 15:56:43 -0600

    Added document onload function

--- app/assets/javascripts/caller.js
+++ app/assets/javascripts/caller.js
@@ -1,7 +1,9 @@
-var peer = new Peer({key: "2pmakgfy6gw7mn29"});
-var remotePeerId = window.location.href.split("/")[4];
+$(function(){
+	var peer = new Peer({key: "2pmakgfy6gw7mn29"});
+	var remotePeerId = window.location.href.split("/")[4];
 
 
-navigator.webkitGetUserMedia({video: true, audio: true}, function(localMediaStream){
-	peer.call(remotePeerId, localMediaStream);
+	navigator.webkitGetUserMedia({video: true, audio: true}, function(localMediaStream){
+		peer.call(remotePeerId, localMediaStream);
+	});
 });
