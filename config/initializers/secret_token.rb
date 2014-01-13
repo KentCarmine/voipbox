96a5c3150998a7cc703359b1fe0f1f0316fbcb19
dbc-apprentice <apprentice@devbootcamp.com>
2014-01-13 13:44:02 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Voipbox::Application.config.secret_key_base = '99f35763ab1ccf0cbde7c2a44339546364d629ee7034f01f0032ddaa464f5d52e43533776cd8856e6a570b2bc43e84ab45894a8c7f3da54ceac630a7c76fce90'
